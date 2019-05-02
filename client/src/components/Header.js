cb82cba11d75ee30dfaf347966d2002d45029850
jonon <jonathancjonsson@outlook.com>
2019-05-02 12:44:32 +0200

    Add Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ComicHaven</h1>
+      </div>
+    );
+  }
+}
