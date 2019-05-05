a4f244831f9e15ae32901f5b2ac93a335acd2655
jonon <jonathancjonsson@outlook.com>
2019-05-05 17:58:01 +0200

    Reset body styling

---
+++ client/src/styles/styles.js
@@ -0,0 +1,33 @@
+const styles = {
+  baseColors: {
+    colors: {
+      primary: {
+        color: "#FFFFFF"
+      }
+    },
+    backgroundColors: {
+      primary: {
+        backgroundColor: "#C61017"
+      }
+    }
+  },
+  layout: {
+    flex: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }
+  },
+  icon: {
+    size: {
+      fontSize: "1.5rem"
+    }
+  },
+
+  title: {
+    fontSize: "18px"
+  }
+};
+
+export default styles;
