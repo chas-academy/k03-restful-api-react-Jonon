56ae0259ed38416a7497964efc20fd4d243fdf67
jonon <jonathancjonsson@outlook.com>
2019-05-06 13:00:16 +0200

    Add footer and make it sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,34 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // components
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Error from "./pages/Error";
+
+// sticky footer
+const container = {
+  display: "flex",
+  minHeight: "100vh",
+  flexDirection: "column"
+};
+
+const maincontent = {
+  flex: "1"
+};
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route component={Error} />
-      </Switch>
-    </Router>
+    <div style={container}>
+      <Router>
+        <Header />
+        <div style={maincontent}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route component={Error} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
