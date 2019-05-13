3ff9bc14c67e2b25ea52a8001091b962b028a82f
jonon <jonathancjonsson@outlook.com>
2019-05-13 13:00:51 +0200

    add external styles and add icon component into Header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
-import Avatar from "./icons/Avatar";
-import Cart from "./icons/Cart";
+import Icon from "./icons/Icon";
 
 // styles
 import Radium from "radium";
-import styles from "../../styles/styles";
+import styles from "./headerStyle";
 import colors from "../../styles/colors";
-
-const header = {
-  base: {
-    padding: "0 1rem 0 1rem",
-    height: "2rem",
-    fontFamily: "Fjalla One, sans-serif",
-    lineHeight: "1"
-  }
-};
 
 class Header extends Component {
   render() {
@@ -24,15 +14,15 @@ class Header extends Component {
       <nav
         style={[
           styles.layout.flex,
-          header.base,
+          styles.base,
           colors.backgroundcolor.primary
         ]}
       >
         <HamburgerMenu />
         <h1 style={[styles.title, colors.color.primary]}>ComicHaven</h1>
         <div style={styles.layout.flex}>
-          <Avatar />
-          <Cart />
+          <Icon icon={"typcn typcn-user-outline"} />
+          <Icon icon={"typcn typcn-shopping-cart"} />
         </div>
       </nav>
     );
---
+++ client/src/components/header/headerStyle.js
@@ -0,0 +1,25 @@
+const styles = {
+  base: {
+    padding: "0 1rem 0 1rem",
+    height: "2rem",
+    fontFamily: "Fjalla One, sans-serif",
+    lineHeight: "1"
+  },
+  layout: {
+    flex: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }
+  },
+  title: {
+    fontSize: "18px"
+  },
+  icon: {
+    color: "#FFFFFF",
+    fontSize: "1.5rem"
+  }
+};
+
+export default styles;
