90f210dcadb376949fd8f6b00deaf8bddf34f442
jonon <jonathancjonsson@outlook.com>
2019-05-17 13:34:04 +0200

    Add slider component

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Slider from "./slider/Slider";
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <Slider />
       </div>
     );
   }
 }
+
+export default Home;
