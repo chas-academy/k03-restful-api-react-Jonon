7009bc456436f2d61b1307b128d571e24348fa86
jonon <jonathancjonsson@outlook.com>
2019-05-18 13:21:55 +0200

    Align bullet at the bottom of the image

--- client/src/components/pages/home/slider/Slider.js
+++ client/src/components/pages/home/slider/Slider.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import image1 from "./images/image1.png";
 import image2 from "./images/image2.jpg";
 import image3 from "./images/image3.jpg";
@@ -8,15 +9,26 @@ import sliderStyle from "./sliderStyle";
 
 const images = [image1, image2, image3];
 
+let list = [];
+
+for (let i = 0; i < images.length; i++) {
+  list.push(<li style={sliderStyle.bullet} />);
+}
+
 class Slider extends Component {
   state = {
-    image: images[0]
+    image: images[2]
   };
 
   render() {
     return (
-      <div style={sliderStyle.imageContainer}>
-        <img src={this.state.image} style={[sliderStyle.image]} />
+      <div style={sliderStyle.divContainer}>
+        <div>
+          <img src={this.state.image} style={sliderStyle.image} />
+          <div style={sliderStyle.controller}>
+            <ul style={sliderStyle.row}>{list}</ul>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/pages/home/slider/sliderStyle.js
+++ client/src/components/pages/home/slider/sliderStyle.js
@@ -1,13 +1,36 @@
 const sliderStyle = {
-  imageContainer: {
+  divContainer: {
+    width: "100%",
     maxHeight: "48.5vw",
+    margin: "auto",
+    textAlign: "center",
+    position: "relative",
     overflow: "hidden"
   },
-
   image: {
     backgroundRepeat: "no-repeat",
     maxWidth: "100%",
     objectFilt: "cover"
+  },
+  controller: {
+    position: "absolute",
+    top: "0.1",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    margin: "0",
+    padding: "0 0 0 0",
+    lineHeight: "0.2"
+  },
+  row: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  bullet: {
+    padding: "0",
+    marginLeft: "1.1rem",
+    marginRight: "1.1rem",
+    fontSize: "2rem"
   }
 };
 export default sliderStyle;
