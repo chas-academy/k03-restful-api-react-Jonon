c20e168d52d8e80ca3a0cdf9262a9123534fa8af
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-06-11 15:48:38 +0200

    Add ReleaseList component

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Slider from "./slider/Slider";
+import ReleasesList from "./releasesList/ReleasesList";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Slider />
+        <ReleasesList />
       </div>
     );
   }
