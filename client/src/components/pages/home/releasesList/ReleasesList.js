bc2734d14197e863fd9ae3ae95aed7d457310bf6
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-07-18 19:52:27 +0200

    Refactor and fix component to change on all screensizes

--- client/src/components/pages/home/releasesList/ReleasesList.js
+++ client/src/components/pages/home/releasesList/ReleasesList.js
@@ -1,107 +1,50 @@
 import React, { Component } from 'react'
 import Icon from '../../../header/icons/Icon';
-import style from "./styles"
 import Radium from "radium";
-import './style.css';
-import MediaQuery from 'react-responsive';
+import MobileList from './list/MobileList';
+import TabletList from './list/TabletList';
+import DesktopList from './list/DesktopList';
 
  class ReleasesList extends Component {
-    constructor(props) {
+        constructor(props) {        
         super(props);
         this.state = {
-            items: [
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/765356/765356._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/232863/232863._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/779028/779028._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/765281/765281._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/460929/460929._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/694446/694446._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/460929/460929._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/755543/755543._SX1280_QL80_TTD_.jpg"},
-               {poster: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/756917/756917._SX1280_QL80_TTD_.jpg"},
-            ],
-            posters: [],
-            windowWith: window.innerWidth,
-            splitIndex: [3,9],
-            
+            width: window.innerWidth
         }
     }
 
-    handleResize = () => {
-        this.setState({ windowWith: window.innerWidth })
-        this.ChangeColumnSize();
-      }
-
-
-      componentDidMount() {
-        this.handleResize()
-        this.removeFromArr();
-        window.addEventListener('resize', this.handleResize,)
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
       }
       
+      componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+      }
+      
+      handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+      };
     
-      componentDidUnmount() {
-        this.handleResize()
-        this.removeFromArr();
-        window.removeEventListener('resize', this.handleResize);
-      }
-
-      removeFromArr() {
-        let posters = this.state.items.splice(this.state.splitIndex[0],this.state.splitIndex[1])
-        console.log("remove")
-        
-      }
-      
-      ChangeColumnSize() {
-        if(this.state.windowWith < 768) {
-            let column = this.state.splitIndex;
-            column[0] = 3;
-            this.setState({splitIndex: column})
-        }
-        else if(this.state.windowWith > 768 && this.state.windowWith < 1024) {
-            let column = this.state.splitIndex;
-            column[0] = 4;
-            this.setState({splitIndex: column})
-        } 
-        else {
-            let column = this.state.splitIndex;
-            column[0] = 5;
-            this.setState({splitIndex: column})
-        }
-        console.log("Add")
-    }
     
     render() {
-        let {items, width, splitIndex} = this.state;
-        let poster = items.map((item, index) => 
-        <img src={item.poster} alt="poster" style={style.poster} key={index}/>
-        )
-        console.log(window.innerWidth)
-
-        
-
-        return (
-            <div style={style.container}>
-                <div>
-                <h2 style={style.title} >New Releases</h2>
-                <div style={[style.posterContainer ]}>
-                    <div 
-                     style={style.posterList} className="posterlist">
-                        {poster}
-                    </div>
-                    <div style={style.buttons}>
-                        <span className="typcn typcn-chevron-left" style={[style.slideButton, style.prevSlide]} ></span>
-                        <span className="typcn typcn-chevron-right" style={[style.slideButton, style.nextSlide]} ></span>
-                    </div>
-                </div>
-                <h3 style={style.viewMoreLink}>View More</h3>
-                </div>
-                <div>
-                    <p>{this.state.splitIndex}</p>
-                    <h1>{this.state.windowWith}</h1>
-                </div>
-            </div>
-        )
+        let {width} = this.state;
+        const isMobile = width <= 768;
+        const isTablet = width > 768 && width <= 1024;
+       
+        if (isMobile) {
+            return (
+                <MobileList />
+              );
+        }
+        if (isTablet) {
+            return (
+                <TabletList />
+              );
+        } else {
+            return (
+                <DesktopList />
+              );
+        }
     }
 }
 
---
+++ client/src/components/pages/home/releasesList/list/DesktopList.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+
+import './style.css';
+import style from './styles';
+import Radium from "radium";
+
+class DesktopList extends Component {
+    constructor(props) {        
+        super(props);
+        this.state = {
+            items: [
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/765356/765356._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/232863/232863._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/779028/779028._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/765281/765281._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/495991/495991._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/694446/694446._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/460929/460929._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/755543/755543._SX1280_QL80_TTD_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/756917/756917._SX1280_QL80_TTD_.jpg",
+            ],
+            posters: [],
+            idx: 0
+        }
+    }
+      
+    sortIntoArray = () => {
+        let posterImages = [...this.state.items];
+        const size = 5;
+        
+        const result = this.state.posters;
+        for (var i=0;i<posterImages.length;i+=size)
+        result.push( posterImages.slice(i,i+size) )
+
+        this.setState({posters: result})
+    }
+
+    componentWillMount() {
+        this.sortIntoArray();
+    }
+
+    nextSlide = () => {
+        let increment = this.state.idx + 1
+
+        this.setState({idx: increment})
+    }
+
+    prevSlide = () => {
+        let increment = this.state.idx - 1
+        this.setState({idx: increment})
+    }
+
+    render() {
+        let {posters, idx} = this.state;
+        let stringifyIdx = idx.toString();
+        
+        let poster = posters[stringifyIdx].map((item, index) => 
+        <img src={item} alt="poster" style={style.poster} key={index}/>
+        )
+
+        return (
+            <div style={style.container}>    
+            <div>
+            <h2 style={style.title} >New Releases</h2>
+            <div style={[style.posterContainer ]}>
+                <div 
+                 style={style.posterList} className="posterlist">
+                    {poster}
+                </div>
+                <div style={style.buttons}>
+                    <span className="typcn typcn-chevron-left" style={[style.slideButton, style.prevSlide]} onClick={this.prevSlide.bind(this)} ></span>
+                    <span className="typcn typcn-chevron-right" style={[style.slideButton, style.nextSlide]} onClick={this.nextSlide.bind(this)} ></span>
+                </div>
+            </div>
+            <h3 style={style.viewMoreLink}>View More</h3>
+            </div>
+            <div>
+                <p>{this.state.splitIndex}</p>
+                <h1>{this.state.windowWith}</h1>
+            </div>
+        </div>
+          );
+    }
+}
+
+export default Radium(DesktopList);
