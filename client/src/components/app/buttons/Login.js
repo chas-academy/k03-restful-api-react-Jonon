2541eb8ffc7ca4b514c8759e539babc24711f505
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-07-28 19:01:12 +0200

    Add styling to button

--- client/src/components/app/buttons/Login.js
+++ client/src/components/app/buttons/Login.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Radium from 'radium';
+import styles from './style'
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                
+            <div style={this.props.styles}>
+                <button style={[styles.base]}>Login</button>
             </div>
         )
     }
 }
 
-export default Login;
+export default Radium(Login);
---
+++ client/src/components/app/buttons/style.js
@@ -0,0 +1,12 @@
+const styles = {
+  base: {
+    border: "none",
+    backgroundColor: "#C61017",
+    color: "#FFFFFF",
+    paddingTop: "0.5rem",
+    paddingBottom: "0.5rem",
+    width: '100%'
+  }
+};
+
+export default styles;
