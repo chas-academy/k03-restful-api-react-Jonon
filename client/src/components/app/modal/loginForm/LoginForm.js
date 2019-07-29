96a89944292a780de969ce6e28f58a1955892ec5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-07-29 19:37:07 +0200

    Add value for width

--- client/src/components/app/modal/loginForm/LoginForm.js
+++ client/src/components/app/modal/loginForm/LoginForm.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import styles from './style';
 import Radium from "radium";
-import Login from '../../buttons/Login';
+import Button from '../../buttons/Button';
 
 class LoginForm extends Component {
+
     render() {
         return (
             <div style={styles.base}>
                 <form style={styles.container}>
                     <input type="text" placeholder="Username or E-mail" style={[styles.input]} />
                     <input type="password" placeholder="Password" style={[styles.input]} />
-                    <Login styles={styles.button} />
+                    <Button title="Login" kind="primary" type="submit" width="91%" />
                 </form>
             </div>
         )
