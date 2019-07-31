8c3c81e8adc34ed646af35dde0d5ab749df5c964
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-07-31 20:36:05 +0200

    Create container for dropdownMenu

--- client/src/components/app/header/dropdownMenu/DropdownMenu.js
+++ client/src/components/app/header/dropdownMenu/DropdownMenu.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Styles from "./style";
+import Search from "./search/Search";
 
- class DropdownMenu extends Component {
-    render() {
-        return (
-            <div style={Styles}>
-                
-            </div>
-        )
-    }
+class DropdownMenu extends Component {
+  render() {
+    return (
+      <div style={Styles.container}>
+        <div style={Styles.base}>
+          <Search />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DropdownMenu;
--- client/src/components/app/header/dropdownMenu/style.js
+++ client/src/components/app/header/dropdownMenu/style.js
@@ -1,9 +1,21 @@
 const styles = {
-    position: 'fixed',
+  base: {
+    position: "fixed",
     zIndex: "11",
     backgroundColor: "#FFFFFF",
-    height: '100%',
-    width: "85%"
-}
+    height: "100%",
+    width: "85%",
+    boxShadow: "1px 0px 4px 0px rgba(0, 0, 0, 0.5)",
+
+  },
+  container: {
+      position: "fixed",
+      zIndex: "13",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%"
+  }
+};
 
 export default styles;
