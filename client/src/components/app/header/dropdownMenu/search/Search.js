99215c53c4be109cb1e3d6830facd101ec8852c1
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-07-31 19:02:56 +0200

    Add default styling to search field

--- client/src/components/app/header/dropdownMenu/search/Search.js
+++ client/src/components/app/header/dropdownMenu/search/Search.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from "./style";
 
 export default class Search extends Component {
     render() {
         return (
             <div>
-                
+                <input style={styles.base} type="text" />
             </div>
         )
     }
---
+++ client/src/components/app/header/dropdownMenu/search/style.js
@@ -0,0 +1,12 @@
+const styles = {
+  base: {
+    width: "99.5%",
+    fontSize: "1.25em",
+    paddingTop: ".5em",
+    paddingBottom: ".5em",
+    border: "1px solid #C61017",
+    borderRadius: "none"
+  }
+};
+
+export default styles;
