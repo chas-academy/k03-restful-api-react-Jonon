24b1505278df7e8670c9ef4aac70b83e8f37167a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-08-07 12:39:54 +0200

    Add Categories

--- client/src/components/app/header/dropdownMenu/DropdownMenu.js
+++ client/src/components/app/header/dropdownMenu/DropdownMenu.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Radium from "radium";
 import Styles from "./style";
 import Search from "./search/Search";
-import Categories from "./categories/categories";
+import Categories from "./categories/categories"; 
 
 class DropdownMenu extends Component {
   render() {
