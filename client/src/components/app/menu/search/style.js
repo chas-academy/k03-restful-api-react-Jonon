4bbf66346eba91f434355a701ccf800b4d61d98e
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-08-10 21:43:33 +0200

    Fix icon position

--- client/src/components/app/menu/search/style.js
+++ client/src/components/app/menu/search/style.js
@@ -1,6 +1,6 @@
 const styles = {
   base: {
-    width: "99.5%",
+    width: "100%%",
     paddingTop: ".5em",
     paddingBottom: ".5em",
     border: "1px solid #C61017",
@@ -9,11 +9,12 @@ const styles = {
     borderRadius: "none",
     border: "none",
     width: "15rem",
-    marginLeft: "1rem",
+    marginLeft: "3rem",
     fontSize: "1.25em",
   },
   searchIcon: {
-    
+    position: "absolute",
+    left: "0",
     marginLeft: "1rem",
     fontSize: "1.5em",
     textAlign: "center",
