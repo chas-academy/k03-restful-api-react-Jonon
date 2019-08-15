a73d111e7fa7658493c265d577a93771a41cef8e
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-08-15 20:27:27 +0200

    Fetch comics

--- client/src/components/app/menu/categories/Subcategories.js
+++ client/src/components/app/menu/categories/Subcategories.js
@@ -6,18 +6,18 @@ class Subcategories extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        { id: 1, title: "Alien", publisher: "Dark Horse" },
-        { id: 2, title: "Predator", publisher: "Dark Horse" },
-        { id: 3, title: "Justice League Dark", publisher: "DC" },
-        { id: 4, title: "Aquaman", publisher: "DC" },
-        { id: 5, title: "Wonder Woman", publisher: "DC" },
-        { id: 6, title: "Batman", publisher: "DC" },
-        { id: 7, title: "Promethee 13:13", publisher: "Delcourt" },
-        { id: 8, title: "Death", publisher: "Vertigo" },
-        { id: 9, title: "Venom vs. Carnage", publisher: "Marvel" }
-      ]
+      items: []
     };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3004/comics")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          items: data
+        });
+      });
   }
 
   render() {
@@ -25,9 +25,7 @@ class Subcategories extends Component {
 
     // Filter to only show publishers comics
     let testing = items.filter(item => {
-      return (
-        item.publisher == this.props.publisher
-      );
+      return item.publisher == this.props.publisher;
     });
 
     let comicList = testing.map(item => {
