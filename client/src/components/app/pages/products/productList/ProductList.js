bf2ea74df7fd618639d0817e3579939dd7176d15
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-08-28 11:17:40 +0200

    Link to comic by id

--- client/src/components/app/pages/products/productList/ProductList.js
+++ client/src/components/app/pages/products/productList/ProductList.js
@@ -3,6 +3,7 @@ import Radium from "radium";
 import style from "./style";
 import Button from "../../../buttons/Button";
 import styles from "../../../buttons/style";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class ProductList extends Component {
   constructor(props) {
@@ -34,11 +35,13 @@ class ProductList extends Component {
           key={item.id}
         >
           <div style={{ flex: "1" }}>
-            <li style={{ padding: "1rem" }} key={item.id}>
-              <img src={item.poster} style={style.image} />
-              <h3 style={[style.posterText, style.title]}>{item.title}</h3>
-              <h4 style={[style.posterText, style.price]}>{item.price}</h4>
-            </li>
+            <Link to={`products/${item.id}`} >
+              <li style={{ padding: "1rem" }} key={item.id}>
+                <img src={item.poster} style={style.image} />
+                <h3 style={[style.posterText, style.title]}>{item.title}</h3>
+                <h4 style={[style.posterText, style.price]}>{item.price}</h4>
+              </li>
+            </Link>
           </div>
           <Button title="Add to cart" kind="primary" width="100%" size="lg" />
         </div>
