0b55f4984b3d7831094488760857526651a0eca6
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-09-08 11:20:55 +0200

    simple setup

--- api/server.js
+++ api/server.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require('cors')
+require("dotenv/config");
 
 //logger
-app.use(morgan('combined'))
+app.use(morgan("dev"));
 
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//cors
+app.use(cors());
 
-const PORT = 3003;
+//Import routes
+const productsRoute = require("./routes/products");
+const orderRoute = require("./routes/orders");
+
+app.use("/products", productsRoute);
+app.use("/orders", orderRoute);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+//connect to DB
+const uri = process.env.DB_CONNECTION;
+
+const db = mongoose.connect(uri, { useNewUrlParser: true }).catch(error => {
+  console.log(error);
+});
+
+const PORT = process.env.PORT || 3003;
 app.listen(`${PORT}`, () => {
   console.log(`server is up and listening on port ${PORT}`);
 });
