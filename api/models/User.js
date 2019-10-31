5568f261e4f3af48e7a48461808bf12fc720cebf
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-10-31 14:20:33 +0100

    Match email so it can't add user without @-sign

--- api/models/User.js
+++ api/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema ({
     name: {type: String, required: true},
     username: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password: {type: String, required: true}
 })
 
