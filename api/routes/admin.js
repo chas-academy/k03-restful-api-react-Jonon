5d7c536ea8607f470b093beea01c8e4bd2494549
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-11-14 19:34:11 +0100

    Admin user can now create products

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,26 +1,67 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
 const Product = require("../models/Product");
 const User = require("../models/User");
 
 // Get all all products
-router.get("/", passport.authenticate("jwt", {session: false}), (req, res) => {
+router.get(
+  "/products",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     const role = req.user.role;
     // Check if user is admin
     if (!role) {
-        return res.status(401).json({Message: "Authentication failed."})
+      return res.status(401).json({ Message: "Authentication failed." });
     }
-    Product.find().then(products => {
-        res.status(200).json({products})
-    }).catch(err => {
-        res.json({message: err})
-    })
-})
-
+    Product.find()
+      .then(products => {
+        res.status(200).json({ products });
+      })
+      .catch(err => {
+        res.json({ message: err });
+      });
+  }
+);
 
 // Create product
+router.post(
+  "/products",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const role = req.user.role;
+    // Check if user is admin
+    if (!role) {
+      return res.status(401).json({ Message: "Authentication failed." });
+    }
+
+    const product = new Product({
+      title: req.body.title,
+      poster: req.body.poster,
+      description: req.body.description,
+      writer: req.body.writer,
+      artist: req.body.artist,
+      coverArtist: req.body.coverArtist,
+      publisher: req.body.publisher,
+      publicationDate: req.body.publicationDate,
+      pages: req.body.pages,
+      price: req.body.price,
+      inventory: req.body.inventory,
+      category: req.body.category,
+      series: req.body.series
+    });
+
+    product
+    .save()
+    .then(doc => {
+      return res.status(201).json(doc);
+    })
+    .catch(err => {
+      res.json({ message: err });
+    });
+  }
+);
 
 // Update product
 
