de3c2faeb3b659f4939ae40e39bfa7eb5fcd3a91
Jonathan Jonsson <jonathancjonsson@outlook.com>
2019-11-14 15:44:23 +0100

    Add role and make username unique

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const userSchema = mongoose.Schema ({
+const userSchema = new Schema({
     name: {type: String, required: true},
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {type: Boolean, default: false}
 })
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = User = mongoose.model("users", userSchema)
