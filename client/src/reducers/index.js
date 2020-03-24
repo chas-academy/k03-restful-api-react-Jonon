8acdd6cd35d0b485a60a7e2d278f6fc8beeee7b9
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-24 11:37:10 +0100

    Add combineReducers and menuReducer into combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
+import  menuReducer from './menuReducer'
 
-
-const rootReducer = () => {}
+const rootReducer = combineReducers({
+    menu: menuReducer
+})
 
 export default rootReducer
