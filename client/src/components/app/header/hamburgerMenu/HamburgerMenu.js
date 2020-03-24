81c2d15e62c6baa4a6be44c317184a05bc69c7e1
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-24 11:43:17 +0100

    Add action for showing menu onclick

--- client/src/components/app/header/hamburgerMenu/HamburgerMenu.js
+++ client/src/components/app/header/hamburgerMenu/HamburgerMenu.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
 import Radium from "radium";
 import colors from "../../styles/colors";
+import { connect } from "react-redux";
+import {show_menu} from '../../../../actions/menuActions'
 
-class HamburgerMenu extends Component {
-  render() {
-    return (
-      <div>
-        <ion-icon
-          style={[colors.color.secondary, { cursor: "pointer" }]}
-          size="large"
-          name="menu"
-          onClick={this.props.showMenu}
-        />
-      </div>
-    );
+const HamburgerMenu = (props) => {
+  return (
+    <div  >
+      <ion-icon 
+        style={[colors.color.secondary, { cursor: "pointer" }]}
+        size="large"
+        name="menu"
+        onClick={props.show_menu}
+      />
+    </div>
+  );
+};
+
+
+const mapDispatchToProps = dispatch => {
+  return{
+    show_menu: () => {dispatch(show_menu)}
   }
 }
 
-export default Radium(HamburgerMenu);
+
+export default connect(null, mapDispatchToProps)(Radium(HamburgerMenu))
