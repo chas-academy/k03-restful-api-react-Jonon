6a3b0fe1b1a77b14dfd06d18fd4e7371e051e205
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-24 11:39:56 +0100

    Create actions and actioncreators

---
+++ client/src/actions/menuActions.js
@@ -0,0 +1,13 @@
+export const SHOW_MENU = "SHOW_MENU";
+export const HIDE_MENU = 'HIDE_MENU';
+
+export const show_menu = {
+  type: "SHOW_MENU",
+};
+
+export const hide_menu = {
+  type: "HIDE_MENU",
+  payload: false
+}
+
+
