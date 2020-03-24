ca7d85d62bcd19a332e0fdf07d2df1c990313c1c
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-24 11:48:11 +0100

    Refactor to redux and add event handler to hide menu when clicking outside

--- client/src/components/app/header/Header.js
+++ client/src/components/app/header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "./hamburgerMenu/HamburgerMenu";
 import UserIconButton from "./userIconButton/UserIconButton";
@@ -9,114 +9,74 @@ import DropdownMenu from "../menu/DropdownMenu";
 import Radium from "radium";
 import styles from "./headerStyle";
 import colors from "../styles/colors";
+import { hide_menu } from "../../../actions/menuActions";
+import { connect } from "react-redux";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-      width: window.innerWidth
+
+
+const Header = (props) => {
+  const node = useRef();
+
+  const handleClick = (e) => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      // stop propagation
+      return false;
+    }
+    // outside click
+    //dispatch action to hide menu
+    props.hide_menu()
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
     };
-  }
+  }, [handleClick]);
 
-  showDropdown = () => {
-    this.setState({
-      show: true
-    });
-  };
+  return (
+    <div>
+      <nav
+        style={[
+          styles.layout.flex,
+          styles.base,
+          colors.backgroundcolor.primary
+        ]}
+      >
+        <div ref={node} style={{height: '100%'}} >
+          <div style={{ marginLeft: "1rem" }}>
+            <HamburgerMenu />
+          </div>
 
-  // hide modal if click outside
-  componentWillMount() {
-    document.addEventListener("click", this.handleClick, false);
-    window.addEventListener("resize", this.handleWindowSizeChange);
-  }
-
-  componentWillUnmount() {
-    document.addEventListener("click", this.handleClick, false);
-    window.removeEventListener("resize", this.handleWindowSizeChange);
-  }
-
-  handleWindowSizeChange = () => {
-    this.setState({ width: window.innerWidth });
-  };
-
-  handleClick = e => {
-    if (this.node.contains(e.target)) {
-      return;
-    }
-    this.setState({ show: false });
-  };
-
-  render() {
-    let { width } = this.state;
-    const isMobileAndIsTablet = width <= 1440;
-
-    if (isMobileAndIsTablet) {
-      return (
-        <div>
-          <nav
-            style={[
-              styles.layout.flex,
-              styles.base,
-              colors.backgroundcolor.primary
-            ]}
-          >
-            <div style={{ marginLeft: "1rem" }}>
-              <HamburgerMenu showMenu={this.showDropdown.bind()} />
-            </div>
-
-            <Link to={"/"}>
-              <h1 style={[styles.title, colors.color.primary]}>ComicHaven</h1>
-            </Link>
-
-            <div style={[styles.layout.flex, { marginRight: "1rem" }]}>
-              <UserIconButton modal={this.props.showModal} />
-              <Cart />
-            </div>
-          </nav>
           {/* push nav */}
           <div style={{ marginTop: "2.5rem" }} />
-          <DropdownMenu
-            show={this.state.show}
-            outside={node => (this.node = node)}
-          />
+          <DropdownMenu />
         </div>
-      );
-    } else {
-      return (
-        <div>
-          <nav
-            style={[
-              styles.layout.flex,
-              styles.base,
-              colors.backgroundcolor.primary
-            ]}
-          >
-            <div style={{ marginLeft: "1rem" }}>
-            <Link to={"/products"}>
-              <h3 style={[colors.color.primary, {fontSize: `1rem`, ":hover": {textDecoration: "underline"}}]} >Comics</h3>
-            </Link>
-            </div>
 
-            <Link to={"/"}>
-              <h1 style={[styles.title, colors.color.primary]}>ComicHaven</h1>
-            </Link>
+        <Link to={"/"}>
+          <h1 style={[styles.title, colors.color.primary]}>ComicHaven</h1>
+        </Link>
 
-            <div style={[styles.layout.flex, { marginRight: "1rem" }]}>
-              <UserIconButton modal={this.props.showModal} />
-              <Cart />
-            </div>
-          </nav>
-          {/* push nav */}
-          <div style={{ marginTop: "2.5rem" }} />
-          <DropdownMenu
-            show={this.state.show}
-            outside={node => (this.node = node)}
-          />
+        <div style={[styles.layout.flex, { marginRight: "1rem" }]}>
+          <UserIconButton modal={props.showModal} />
+          <Cart />
         </div>
-      );
-    }
+      </nav>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    menu: state.menu
   }
 }
 
-export default Radium(Header);
+const mapDispatchToProps = dispatch => {
+  return{
+    hide_menu: () => {dispatch(hide_menu)}
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Radium(Header));
