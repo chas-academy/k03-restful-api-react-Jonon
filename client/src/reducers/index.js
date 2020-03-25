e3fd2568549319c0740c1d0627da544d98c9e025
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-25 12:23:44 +0100

    Add import modalReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
-import  menuReducer from './menuReducer'
+import menuReducer from './menuReducer'
+import modalReducer from './modalReducer'
 
 const rootReducer = combineReducers({
-    menu: menuReducer
+    menu: menuReducer,
+    modal: modalReducer
 })
 
 export default rootReducer
