537bc31116f844381e6fab3412c5456ed111baa2
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-25 12:22:05 +0100

    Add ModalReducer

---
+++ client/src/reducers/modalReducer.js
@@ -0,0 +1,22 @@
+import { SHOW_MODAL, HIDE_MODAL } from "../actions/modalActions";
+
+const initialState = {
+    payload: false
+};
+
+const modalReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SHOW_MODAL:
+      return {
+          payload: action.payload
+      };
+    case HIDE_MODAL:
+      return {
+          payload: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default modalReducer
