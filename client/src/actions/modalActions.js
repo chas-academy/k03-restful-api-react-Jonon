be4f9d5969c066d8f0f37ff4b7b9a353e8e53ae0
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-03-25 12:22:55 +0100

    Add actions and action creators for Modal

---
+++ client/src/actions/modalActions.js
@@ -0,0 +1,12 @@
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
+
+export const show_modal = {
+    type: "SHOW_MODAL",
+    payload: true
+}
+
+export const hide_modal = {
+    type: "HIDE_MODAL",
+    payload: false
+}
