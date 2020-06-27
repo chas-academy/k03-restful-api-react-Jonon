67902eb3a3e008bcb712519f1b20ff425924e775
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-06-27 21:12:29 +0200

    Refactor to use hooks

--- client/src/components/pages/home/Carousel/Carousel.js
+++ client/src/components/pages/home/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import Radium from "radium";
 import image1 from "./images/image1.png";
@@ -10,67 +10,88 @@ import image6 from "./images/image6.jpg";
 // styles
 import carouselStyle from "./carouselStyle";
 
-const timeBetweenImages = 5000;
+const timeBetweenImages = 3000;
 
 const Carousel = () => {
+  const [carousel, setCarousel] = useState({
+    images: [image1, image2, image3, image4, image5, image6],
+    idx: 0,
+    selected: {
+      color: `white`,
+    },
+    notSelected: {
+      color: `rgba(256, 256, 256, 0.6)`,
+    },
+  });
 
-    this.state = {
-      images: [image1, image2, image3, image4, image5, image6],
-      idx: 0,
-      selected: {
-        color: `white`,
-      },
-      notSelected: {
-        color: `rgba(256, 256, 256, 0.6)`,
-      },
-    };
-  }
+  const nextImage = () => {
+    // const current = carousel.idx.length;
+    // let next = carousel.idx++;
 
-  onClickChangeActiveImage = (index) => this.setState({ idx: index });
-
-  componentDidMount() {
-    setInterval(() => this.nextImage(), timeBetweenImages);
-  }
-
-  nextImage() {
-    let current = this.state.idx;
-    let next = ++current;
-
-    if (next >= this.state.images.length) {
+    /* if (next >= carousel.images.length) {
       next = 0;
+    } */
+
+    setCarousel((currentState) => {
+      return {
+        ...currentState,
+        idx: carousel.idx + 1,
+      };
+    });
+
+    // prevent subtract to prevent index from counting beyond the amount of images
+    if (carousel.idx > carousel.images.length - 2) {
+      setCarousel((currentState) => {
+        return {
+          ...currentState,
+          idx: 0,
+        };
+      });
     }
+  };
 
-    this.setState({ idx: next });
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextImage();
+    }, timeBetweenImages);
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
-  render() {
-    const { images } = this.state;
+  const onClickChangeActiveImage = (index) =>
+    setCarousel((currentState) => {
+      return {
+        ...currentState,
+        idx: index,
+      };
+    });
 
-    const controller = images.map((control, index) => (
-      <li
-        key={index}
-        onClick={this.onClickChangeActiveImage.bind(this, index)}
-        style={[
-          carouselStyle.bullet,
-          this.state.idx !== index ? this.state.notSelected : this.state.selected,
-        ]}
-      />
-    ));
+  const controller = carousel.images.map((controller, index) => (
+    <li
+      key={index}
+      onClick={() => onClickChangeActiveImage(index)}
+      style={[
+        carouselStyle.bullet,
+        carousel.idx !== index ? carousel.notSelected : carousel.selected,
+      ]}
+    />
+  ));
 
-    return (
-      <div style={carouselStyle.divContainer}>
-        <div>
-          <img
-            src={this.state.images[this.state.idx]}
-            style={carouselStyle.image}
-            alt=""
-          />
-          <div style={carouselStyle.controller}>
-            <ul style={carouselStyle.row}>{controller}</ul>
-          </div>
+  return (
+    <div style={carouselStyle.divContainer}>
+      <div>
+        <img
+          src={carousel.images[carousel.idx]}
+          style={carouselStyle.image}
+          alt=""
+        />
+        <div style={carouselStyle.controller}>
+          <ul style={carouselStyle.row}>{controller}</ul>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
 export default Radium(Carousel);
