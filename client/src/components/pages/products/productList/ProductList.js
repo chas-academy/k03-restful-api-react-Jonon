2414a010c75cbd91492d64159186fa2c76c67684
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-07-05 16:36:21 +0200

    Refactor to use hooks

--- client/src/components/pages/products/productList/ProductList.js
+++ client/src/components/pages/products/productList/ProductList.js
@@ -1,58 +1,50 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Radium from "radium";
 import { Link } from "react-router-dom";
 import style from "./style";
 import Button from "../../../buttons/Button";
 
-class ProductList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-    };
-  }
+const ProductList = (props) => {
+  const [items, setItems] = useState({
+    items: [],
+  });
 
-  componentDidMount() {
-    fetch(`${this.props.location.pathname}`)
+  useEffect(() => {
+    fetch(`${props.location.pathname}`)
       .then((res) => res.json())
       .then((data) => {
-        this.setState({
+        setItems({
           items: data,
         });
       });
-  }
+  }, []);
 
-  render() {
-    const { items } = this.state;
-    const comics = items.map((item) => {
-      return (
-        <div
-          style={[{ display: "flex", flexDirection: "column" }, style.posterBox]}
-          key={item._id}
-        >
-          <div style={{ flex: "1" }}>
-            <Link to={`${this.props.location.pathname}/${item._id}`}>
-              <li style={{ padding: "1rem" }} key={item._id}>
-                <img src={item.poster} style={style.image} alt="poster" />
-                <h3 style={[style.posterText, style.title]}>{item.title}</h3>
-                <h4 style={[style.posterText, style.price]}>{item.price}
-€
-</h4>
-              </li>
-            </Link>
-          </div>
-          <Button title="Add to cart" kind="primary" width="100%" size="lg" />
-        </div>
-      );
-    });
+  const comics = items.items.map((item) => {
     return (
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>
-          <ul style={[style.posterContainer]}>{comics}</ul>
+      <div
+        style={[{ display: "flex", flexDirection: "column" }, style.posterBox]}
+        key={item._id}
+      >
+        <div style={{ flex: "1" }}>
+          <Link to={`${props.location.pathname}/${item._id}`}>
+            <li style={{ padding: "1rem" }} key={item._id}>
+              <img src={item.poster} style={style.image} alt="poster" />
+              <h3 style={[style.posterText, style.title]}>{item.title}</h3>
+              <h4 style={[style.posterText, style.price]}>{item.price}€</h4>
+            </li>
+          </Link>
         </div>
+        <Button title="Add to cart" kind="primary" width="100%" size="lg" />
       </div>
     );
-  }
-}
+  });
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        <ul style={[style.posterContainer]}>{comics}</ul>
+      </div>
+    </div>
+  );
+};
 
 export default Radium(ProductList);
