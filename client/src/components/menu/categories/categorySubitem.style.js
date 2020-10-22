b0bfa19759038d10b3bc4ac8977f8b7c3cd09123
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-10-22 20:50:10 +0200

    Add styling to categorySubitem

---
+++ client/src/components/menu/categories/categorySubitem.style.js
@@ -0,0 +1,19 @@
+const styles = {
+  item: {
+    paddingLeft: "2rem",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
+    lineHeight: "2rem",
+    cursor: "pointer",
+    fontFamily: "Roboto, sans-serif",
+  },
+  subCategory: {
+    fontWeight: "200",
+    backgroundColor: "#FAFAFA",
+    ":hover": {
+      backgroundColor: "#F0F0F0",
+    },
+  },
+};
+
+export default styles;
