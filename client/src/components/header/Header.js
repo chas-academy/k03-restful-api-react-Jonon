2130d216dd69ca503c3875d3f30355c90fc995f1
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-02 15:35:57 +0100

    Remove ref

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Radium from "radium";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import HamburgerMenu from "./hamburgerMenu/HamburgerMenu";
 import UserIconButton from "./userIconButton/UserIconButton";
 import Cart from "./cart/Cart";
@@ -12,36 +10,12 @@ import CategoryItem from "../menu/categories/categoryItem";
 // styles
 import styles from "./headerStyle";
 import colors from "../styles/colors";
-import { hide_menu } from "../../actions/menuActions";
 
-const Header = (props) => {
-  const node = useRef();
-
-  const handleClick = (e) => {
-    if (node.current.contains(e.target)) {
-      // inside click
-      // stop propagation
-      return false;
-    }
-    // outside click
-    // check if menu is open
-    if (props.menu.payload) {
-      // dispatch action to hide menu
-      props.hide_menu();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  });
-
+const Header = () => {
   return (
     <div>
       <nav style={[styles.layout.flex, styles.base, colors.backgroundcolor.primary]}>
-        <div ref={node} style={{ height: "100%" }}>
+        <div style={{ height: "100%" }}>
           <div style={{ marginLeft: "1rem" }}>
             <HamburgerMenu />
           </div>
@@ -64,23 +38,4 @@ const Header = (props) => {
   );
 };
 
-Header.propTypes = {
-  menu: PropTypes.func.isRequired,
-  hide_menu: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = (state) => {
-  return {
-    menu: state.menu,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    hide_menu: () => {
-      dispatch(hide_menu);
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Radium(Header));
+export default Radium(Header);
