11350f8863fd161a1d47dce5a45d305f3184806a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-08 14:13:42 +0100

    Add svg

--- client/src/components/header/Logo.js
+++ client/src/components/header/Logo.js
@@ -9,7 +9,22 @@ const Logo = () => {
   return (
     <div>
       <Link to="/">
-        <h1 style={[styles.title, colors.color.primary]}>ComicHaven</h1>
+        <div style={styles.root}>
+          <h1 style={[styles.title, colors.color.primary]}>Comic</h1>
+          <svg
+            width="18"
+            height="35"
+            viewBox="0 0 129 197"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M63.5 0L0 130H28.5L37.5 111.5H78.5L63.5 142.5V197L128.5 68H99L89.5 87.5H48L63.5 54.5V0Z"
+              fill="#FFFFFF"
+            />
+          </svg>
+          <h1 style={[styles.title, colors.color.primary]}>aven</h1>
+        </div>
       </Link>
     </div>
   );
