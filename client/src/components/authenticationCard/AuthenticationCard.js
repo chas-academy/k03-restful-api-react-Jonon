77b4a0c6f63e9e017820e0557f68d84c4647d272
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-10 10:25:22 +0100

    Add logo and sizing to card

--- client/src/components/authenticationCard/AuthenticationCard.js
+++ client/src/components/authenticationCard/AuthenticationCard.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
+import Radium from "radium";
+// Hooks
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+// Components
 import Button from "../buttons/Button";
 import Login from "./login/Login";
 import Register from "./register/Register";
+import Logo from "./Logo";
+// styles
+import Styles from "./AuthenticationCard.style";
+import colors from "../buttons/style";
 
 const AuthenticationCard = () => {
   // false = login
   // true = register
   const [activeTab, setActiveTab] = useState(false);
 
+  const size = useWindowDimensions();
+  const boxSize = {
+    height: `${size.height}px`,
+    width: `${size.width}px`,
+    "@media (min-width: 768px)": {
+      height: "20rem",
+      width: "25rem",
+    },
+  };
   return (
-    <div>
+    <div style={boxSize}>
       <Button
         title="Login"
         width="50%"
@@ -26,9 +43,10 @@ const AuthenticationCard = () => {
         size="md"
         onClick={() => setActiveTab(true)}
       />
+      <Logo />
       {!activeTab ? <Login /> : <Register />}
     </div>
   );
 };
 
-export default AuthenticationCard;
+export default Radium(AuthenticationCard);
