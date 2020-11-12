a73b5543d53d2c8c5dea23631eb5600c85a3b099
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-12 15:48:04 +0100

    Add styling to CloseButton

---
+++ client/src/components/authenticationCard/closeButton/closeButton.style.js
@@ -0,0 +1,13 @@
+const { default: style } = require("../../../assets/form/input.style");
+
+const styles = {
+  cursor: "pointer",
+  fontFamily: "Roboto, sans-serif",
+  color: "#3F474A",
+  ":hover": {
+    color: "#007CC2",
+  },
+  fontWeight: "400",
+};
+
+export default styles;
