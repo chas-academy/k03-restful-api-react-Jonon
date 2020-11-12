a43f22850b7682ab49c39b3043037191b298c46c
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-12 10:38:13 +0100

    Add button variable. Add contional on button size.

--- client/src/components/authenticationCard/login/Login.js
+++ client/src/components/authenticationCard/login/Login.js
@@ -1,23 +1,35 @@
 import React from "react";
 import Radium from "radium";
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
+// styles
 import styles from "./style";
+// components
 import Button from "../../buttons/Button";
 
 const Login = () => {
+  const window = useWindowDimensions();
+  const { width } = window;
+  const isMobile = width <= 768;
+
+  const button = (
+    <Button
+      title="Login"
+      kind="primary"
+      type="submit"
+      width="90%"
+      marginTop="2rem"
+      size={isMobile ? "lg" : "md"}
+      padding="1rem"
+      fontSize="1rem"
+    />
+  );
+
   return (
     <div style={styles.base}>
       <form style={styles.container}>
         <input type="text" placeholder="Username or E-mail" style={[styles.input]} />
         <input type="password" placeholder="Password" style={[styles.input]} />
-        <Button
-          title="Login"
-          kind="primary"
-          type="submit"
-          width="91%"
-          marginTop="2rem"
-          size="lg"
-          fontSize="1rem"
-        />
+        {button}
         <p>Cancel</p>
       </form>
     </div>
--- client/src/components/authenticationCard/login/style.js
+++ client/src/components/authenticationCard/login/style.js
@@ -7,16 +7,6 @@ const styles = {
     marginTop: "1rem",
     paddingBottom: "2rem",
   },
-  input: {
-    borderRadius: "0.2rem",
-    marginTop: "1rem",
-    border: ".01px solid #4B3A39",
-    width: "90%",
-    paddingTop: "0.5rem",
-    paddingBottom: "0.5rem",
-    fontSize: "1rem",
-    fontFamily: "Roboto, sans-serif",
-  },
 };
 
 export default styles;
