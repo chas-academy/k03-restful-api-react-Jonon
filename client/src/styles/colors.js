02698ef0a8c0d8571b7fec14ebc74c8ae582cd7f
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-13 17:08:20 +0100

    Add new colors

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,16 +1,21 @@
 const colors = {
   // Thunderbird
   primary: "#C61017",
+  /* ------------------------------------------------------- */
   // Lochmara
   secondary: "#007CC2",
+  /* ------------------------------------------------------- */
   // Cape Cod
   tertiary: "#3F474A",
   // Mine Shaft
   teriaryVariant: "#303030",
+  // Matterhorn
+  teriaryVariant2: "#4B3A39",
+  /* ------------------------------------------------------- */
   // White
   quaternary: "#FFFFFF",
   // Alabaster
-  quinary: "FAFAFA",
+  quaternaryVariant: "FAFAFA",
 };
 
 export default colors;
