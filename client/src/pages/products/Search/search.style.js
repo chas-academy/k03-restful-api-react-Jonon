42392a78b4cc2931ac772a5862c8798679f3151a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-19 14:06:37 +0100

    Align icon beside the search field

---
+++ client/src/pages/products/Search/search.style.js
@@ -0,0 +1,17 @@
+const styles = {
+  row: {
+    display: "grid",
+    gridTemplateColumns: "3fr 1.5rem",
+    border: "1px solid red",
+    width: "95%",
+    marginTop: "1rem",
+    borderRadius: "0.2rem",
+  },
+
+  verticalAlign: {
+    display: "flex",
+    alignItems: "center",
+  },
+};
+
+export default styles;
