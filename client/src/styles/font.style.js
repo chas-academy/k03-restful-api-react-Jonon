b55cced46f24d66f325ff95616a8b458208a6b96
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-11-30 14:07:57 +0100

    Create font styles

---
+++ client/src/styles/font.style.js
@@ -0,0 +1,16 @@
+const font = {
+  family: {
+    archivoNarrow: "Archivo Narrow, sans-serif",
+  },
+  weight: {
+    thin: 200,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 900,
+  },
+};
+
+export default font;
