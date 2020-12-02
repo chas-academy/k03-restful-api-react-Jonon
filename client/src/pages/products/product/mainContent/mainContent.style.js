4cb2eba4994935772ef265cb45431f55b8906f54
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-12-02 10:38:32 +0100

    Add font styling

--- client/src/pages/products/product/mainContent/mainContent.style.js
+++ client/src/pages/products/product/mainContent/mainContent.style.js
@@ -1,12 +1,19 @@
+import font from "../../../../styles/font.style";
+import color from "../../../../styles/colors";
+
 const styles = {
   image: {
     width: "100%",
   },
   title: {
-    fontFamily: "Archivo Narrow, sans-serif",
+    fontFamily: `${font.family.archivoNarrow}`,
+    fontWeight: `${font.weight.semiBold}`,
+    color: `${color.greys.variant00}`,
   },
   price: {
     fontFamily: "Archivo Narrow, sans-serif",
+    fontWeight: `${font.weight.medium}`,
+    color: `${color.greys.variant00}`,
   },
 };
 
