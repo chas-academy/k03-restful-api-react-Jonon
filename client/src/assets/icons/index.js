daa92b5b687a579618c79062aa8c8f8d8ee7e35c
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-12-09 19:38:48 +0100

    Add social icons

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Cart, User, UserOutline, Logo, ChevronRight } from "./icons";
+import {
+  Cart,
+  User,
+  UserOutline,
+  Logo,
+  ChevronRight,
+  Facebook,
+  Twitter,
+  Instagram,
+  YouTube,
+} from "./icons";
 
 const Icon = ({ width, height, icon }) => {
   switch (icon.toLowerCase()) {
@@ -13,6 +23,14 @@ const Icon = ({ width, height, icon }) => {
       return <Logo width={width} height={height} />;
     case "chevronright":
       return <ChevronRight width={width} height={height} />;
+    case "facebook":
+      return <Facebook width={width} height={height} />;
+    case "twitter":
+      return <Twitter width={width} height={height} />;
+    case "instagram":
+      return <Instagram width={width} height={height} />;
+    case "youtube":
+      return <YouTube width={width} height={height} />;
   }
 };
 
