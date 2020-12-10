3214e74b1a52e7b3ac7faa01e66a805dd59cc656
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-12-10 17:42:59 +0100

    Add custom style prop

--- client/src/assets/icons/icons.js
+++ client/src/assets/icons/icons.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Radium from "radium";
 import styles from "./icon.style";
 
-export const Cart = ({ width, height, fill }) => {
+export const Cart = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       version="1.2"
@@ -10,7 +11,7 @@ export const Cart = ({ width, height, fill }) => {
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <g>
         <path d="M20.756 5.345c-.191-.219-.466-.345-.756-.345h-13.819l-.195-1.164c-.08-.482-.497-.836-.986-.836h-2.25c-.553 0-1 .447-1 1s.447 1 1 1h1.403l1.86 11.164.045.124.054.151.12.179.095.112.193.13.112.065c.116.047.238.075.367.075h11.001c.553 0 1-.447 1-1s-.447-1-1-1h-10.153l-.166-1h11.319c.498 0 .92-.366.99-.858l1-7c.041-.288-.045-.579-.234-.797zm-1.909 1.655l-.285 2h-3.562v-2h3.847zm-4.847 0v2h-3v-2h3zm0 3v2h-3v-2h3zm-4-3v2h-3l-.148.03-.338-2.03h3.486zm-2.986 3h2.986v2h-2.653l-.333-2zm7.986 2v-2h3.418l-.285 2h-3.133z" />
@@ -21,7 +22,7 @@ export const Cart = ({ width, height, fill }) => {
   );
 };
 
-export const Search = ({ width, height, fill }) => {
+export const Search = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +30,7 @@ export const Search = ({ width, height, fill }) => {
       viewBox="0 0 512 512"
       width={width}
       height={height}
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <title>Search</title>
       <path
@@ -51,7 +52,7 @@ export const Search = ({ width, height, fill }) => {
   );
 };
 
-export const User = ({ width, height, fill }) => {
+export const User = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       version="1.2"
@@ -60,14 +61,14 @@ export const User = ({ width, height, fill }) => {
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <path d="M17 9c0-1.381-.56-2.631-1.464-3.535-.905-.905-2.155-1.465-3.536-1.465s-2.631.56-3.536 1.465c-.904.904-1.464 2.154-1.464 3.535s.56 2.631 1.464 3.535c.905.905 2.155 1.465 3.536 1.465s2.631-.56 3.536-1.465c.904-.904 1.464-2.154 1.464-3.535zM6 19c0 1 2.25 2 6 2 3.518 0 6-1 6-2 0-2-2.354-4-6-4-3.75 0-6 2-6 4z" />
     </svg>
   );
 };
 
-export const UserOutline = ({ width, height, fill }) => {
+export const UserOutline = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       version="1.2"
@@ -76,14 +77,14 @@ export const UserOutline = ({ width, height, fill }) => {
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <path d="M12 6c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3m0-2c-2.764 0-5 2.238-5 5s2.236 5 5 5 5-2.238 5-5-2.236-5-5-5zM12 17c2.021 0 3.301.771 3.783 1.445-.683.26-1.969.555-3.783.555-1.984 0-3.206-.305-3.818-.542.459-.715 1.777-1.458 3.818-1.458m0-2c-3.75 0-6 2-6 4 0 1 2.25 2 6 2 3.518 0 6-1 6-2 0-2-2.354-4-6-4z" />
     </svg>
   );
 };
 
-export const Logo = ({ width, height, fill }) => {
+export const Logo = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       // default size should be width 24 and height 37
@@ -91,7 +92,7 @@ export const Logo = ({ width, height, fill }) => {
       height={height}
       viewBox="0 0 24 37"
       fill="none"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
@@ -102,7 +103,7 @@ export const Logo = ({ width, height, fill }) => {
   );
 };
 
-export const ChevronRight = ({ width, height, fill }) => {
+export const ChevronRight = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       version="1.2"
@@ -111,21 +112,21 @@ export const ChevronRight = ({ width, height, fill }) => {
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <path d="M8.586 5.586c-.781.781-.781 2.047 0 2.828l3.585 3.586-3.585 3.586c-.781.781-.781 2.047 0 2.828.39.391.902.586 1.414.586s1.024-.195 1.414-.586l6.415-6.414-6.415-6.414c-.78-.781-2.048-.781-2.828 0z" />
     </svg>
   );
 };
 
-export const Facebook = ({ width, height, fill }) => {
+export const Facebook = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 320 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       {/* Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) */}
       <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
@@ -133,14 +134,14 @@ export const Facebook = ({ width, height, fill }) => {
   );
 };
 
-export const Twitter = ({ width, height, fill }) => {
+export const Twitter = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 512 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       {/* Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) */}
       <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
@@ -148,14 +149,14 @@ export const Twitter = ({ width, height, fill }) => {
   );
 };
 
-export const Instagram = ({ width, height, fill }) => {
+export const Instagram = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 448 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       {/* Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) */}
       <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
@@ -163,14 +164,14 @@ export const Instagram = ({ width, height, fill }) => {
   );
 };
 
-export const YouTube = ({ width, height, fill }) => {
+export const YouTube = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       viewBox="0 0 576 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       {/* Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) */}
       <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
@@ -178,7 +179,7 @@ export const YouTube = ({ width, height, fill }) => {
   );
 };
 
-export const Menu = ({ width, height, fill }) => {
+export const Menu = Radium(({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -186,15 +187,15 @@ export const Menu = ({ width, height, fill }) => {
       height={height}
       class="ionicon"
       viewBox="0 0 512 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <title>Menu</title>
       <path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z" />
     </svg>
   );
-};
+});
 
-export const Star = ({ width, height, fill }) => {
+export const Star = ({ width, height, fill, customStyle }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -202,7 +203,7 @@ export const Star = ({ width, height, fill }) => {
       height={height}
       class="ionicon"
       viewBox="0 0 512 512"
-      style={styles.fill[fill] || fill}
+      style={[styles.fill[fill], fill, customStyle]}
     >
       <title>Star</title>
       <path d="M496 203.3H312.36L256 32l-56.36 171.3H16l150.21 105.4-58.5 171.3L256 373.84 404.29 480l-58.61-171.3z" />
--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -14,30 +14,87 @@ import {
   Star,
 } from "./icons";
 
-const Icon = ({ width, height, icon, fill }) => {
+const Icon = ({ width, height, icon, fill, customStyle }) => {
   switch (icon.toLowerCase()) {
     case "cart":
-      return <Cart width={width} height={height} />;
+      return (
+        <Cart width={width} height={height} fill={fill} customStyle={customStyle} />
+      );
     case "search":
-      return <Search width={width} height={height} />;
+      return (
+        <Search
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "user":
-      return <User width={width} height={height} />;
+      return (
+        <User width={width} height={height} fill={fill} customStyle={customStyle} />
+      );
     case "useroutline":
-      return <UserOutline width={width} height={height} />;
+      return (
+        <UserOutline
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "logo":
-      return <Logo width={width} height={height} />;
+      return (
+        <Logo width={width} height={height} fill={fill} customStyle={customStyle} />
+      );
     case "chevronright":
-      return <ChevronRight width={width} height={height} />;
+      return (
+        <ChevronRight
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "facebook":
-      return <Facebook width={width} height={height} />;
+      return (
+        <Facebook
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "twitter":
-      return <Twitter width={width} height={height} />;
+      return (
+        <Twitter
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "instagram":
-      return <Instagram width={width} height={height} />;
+      return (
+        <Instagram
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "youtube":
-      return <YouTube width={width} height={height} />;
+      return (
+        <YouTube
+          width={width}
+          height={height}
+          fill={fill}
+          customStyle={customStyle}
+        />
+      );
     case "menu":
-      return <Menu width={width} height={height} />;
+      return (
+        <Menu width={width} height={height} fill={fill} customStyle={customStyle} />
+      );
     case "star":
       return <Star width={width} height={height} fill={fill} />;
   }
