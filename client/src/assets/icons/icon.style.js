7e75576a3956a5eaa1efa95f33ed6c44754cc672
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-12-10 15:48:31 +0100

    Add default colors

---
+++ client/src/assets/icons/icon.style.js
@@ -0,0 +1,14 @@
+import color from "../../styles/colors";
+
+const styles = {
+  fill: {
+    primary: {
+      fill: color.shades.grey00,
+    },
+    secondary: {
+      fill: color.shades.white00,
+    },
+  },
+};
+
+export default styles;
