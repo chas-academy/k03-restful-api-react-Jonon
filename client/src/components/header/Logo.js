ff5c57baf927336207b0cfd1bc51b7a267c67729
Jonathan Jonsson <jonathancjonsson@outlook.com>
2020-12-12 15:07:11 +0100

    Update icon to use icon from library instead

--- client/src/components/header/Logo.js
+++ client/src/components/header/Logo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Radium from "radium";
+// Assets
+import Icon from "../../assets/icons";
 // styles
 import styles from "./logo.style";
 import colors from "../../styles/colors";
@@ -11,18 +13,7 @@ const Logo = () => {
     <Link style={link.noUnderline} to="/">
       <div style={styles.alignCenter}>
         <h1 style={[styles.title, { color: colors.shades.white00 }]}>Comic</h1>
-        <svg
-          width="23"
-          height="40"
-          viewBox="0 0 129 197"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M63.5 0L0 130H28.5L37.5 111.5H78.5L63.5 142.5V197L128.5 68H99L89.5 87.5H48L63.5 54.5V0Z"
-            fill={colors.shades.white00}
-          />
-        </svg>
+        <Icon icon="logo" width="32" fill="secondary" />
         <h1 style={[styles.title, { color: colors.shades.white00 }]}>aven</h1>
       </div>
     </Link>
