8e05320cb548df3773ce56a0b1b597c68d68ea93
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-01-13 15:49:36 +0100

    Add check if prop is array

--- client/src/pages/products/product/credits/CreditsList.js
+++ client/src/pages/products/product/credits/CreditsList.js
@@ -2,15 +2,20 @@ import React from "react";
 import styles from "./Credits.style";
 
 const CreditsList = ({ items, heading }) => {
+  const checkType = (data) => {
+    if (!data) {
+      return <p>Loading</p>;
+    } else if (Array.isArray(data)) {
+      return data.map((w, idx) => <p key={idx}>{w.name}</p>);
+    } else if (typeof data === "string") {
+      return <p>{data}</p>;
+    }
+  };
+
   return (
     <div>
       <h2 style={styles.listHeader}>{heading}</h2>
-
-      {!items || !items.writer ? (
-        <p>Loading</p>
-      ) : (
-        items["writer"].map((w, idx) => <p key={idx}>{w.name}</p>)
-      )}
+      {checkType(items)}
     </div>
   );
 };
