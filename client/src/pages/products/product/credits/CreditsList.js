1fb778cf6344a85ec81d5e3cd363b3889b7b5884
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-01-26 15:54:16 +0100

    Load object and check if it's an array, string, or number and convert pubDate to a readable string.

--- client/src/pages/products/product/credits/CreditsList.js
+++ client/src/pages/products/product/credits/CreditsList.js
@@ -1,22 +1,34 @@
 import React from "react";
-import styles from "./Credits.style";
+import styles from "./CreditList.style";
 
-const CreditsList = ({ items, heading }) => {
-  const checkType = (data) => {
-    if (!data) {
+const CreditsList = ({ items, pubDate, heading }) => {
+  const checkType = (data, pubDate) => {
+    // Check if data and pubDate is loaded
+    if (!data && !pubDate) {
       return <p>Loading</p>;
+      // Check if it's an array
     } else if (Array.isArray(data)) {
-      return data.map((w, idx) => <p key={idx}>{w.name}</p>);
-    } else if (typeof data === "string") {
-      return <p>{data}</p>;
+      return data.map((w, idx) => (
+        <li style={styles.itemHeader} key={idx}>
+          {w.name}
+        </li>
+      ));
+      // check if data is a string or a number
+    } else if (typeof data === "string" || typeof data === "number") {
+      return <li style={styles.itemHeader}>{data}</li>;
+    } else if (pubDate) {
+      // Convert pubDate to a date string
+      let publicationDate = pubDate;
+      publicationDate = new Date(publicationDate).toDateString();
+      return <li>{publicationDate}</li>;
     }
   };
 
   return (
-    <div>
-      <h2 style={styles.listHeader}>{heading}</h2>
-      {checkType(items)}
-    </div>
+    <>
+      <h2 style={styles.header}>{heading}</h2>
+      {checkType(items, pubDate)}
+    </>
   );
 };
 
