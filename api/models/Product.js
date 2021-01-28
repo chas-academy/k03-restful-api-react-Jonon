bede9ca35cd0a900f7aedc3e3f5c0fbe6e72e4c5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-01-28 15:36:20 +0100

    Update model

--- api/models/Product.js
+++ api/models/Product.js
@@ -4,16 +4,28 @@ const productSchema = mongoose.Schema({
   title: { type: String, required: true },
   poster: { type: String, required: true },
   description: { type: String, required: true },
-  writer: { type: String, required: true },
-  artist: { type: String, required: true },
-  coverArtist: { type: String, required: true },
+  writer: [
+    {
+      name: { type: String },
+    },
+  ],
+  artist: [
+    {
+      name: { type: String },
+    },
+  ],
+  coverArtist: [
+    {
+      name: { type: String },
+    },
+  ],
   publisher: { type: String, required: true },
-  publicationDate: { type: Date, default: Date.now },
+  publicationDate: { type: Date, default: Date.now() },
   pages: { type: Number, required: true },
   price: { type: Number, required: true },
-  inventory: { type: Number, required: true, default: 0  },
+  inventory: { type: Number, required: true, default: 0 },
   category: [String],
-  series: {type: String}
+  series: { type: String },
 });
 
 module.exports = mongoose.model("Product", productSchema);
