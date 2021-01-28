f260fb8602672abdd4d1c0d9f962312ffdf84b49
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-01-28 15:39:45 +0100

    Update Category model

--- api/models/Category.js
+++ api/models/Category.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
   title: String,
-  subcategories: [{
-    title: {type: String}
-}]
+  subcategories: [
+    {
+      title: { type: String },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
