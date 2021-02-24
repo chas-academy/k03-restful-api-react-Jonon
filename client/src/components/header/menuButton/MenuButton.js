3a99ae3e5d5c61add358ed80f54cf8c3e2a638b5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-02-24 20:29:04 +0100

    Rename component and add check if user is authenticated and change button acordingly

---
+++ client/src/components/header/menuButton/MenuButton.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+// assets
+import Icon from "../../../assets/icons";
+// styles
+import userIconButtonStyle from "./MenuButton.style";
+import { show_modal } from "../../../actions/modalActions";
+
+const MenuButton = ({ show_modal }) => {
+  let auth = useSelector((state) => state.authentication.auth);
+
+  const [hover, setHover] = useState({ onHover: "useroutline" });
+
+  const handleMouseEnter = () => {
+    setHover({ onHover: "user" });
+  };
+
+  const handleMouseLeave = () => {
+    setHover({ onHover: "useroutline" });
+  };
+
+  return (
+    <div style={userIconButtonStyle}>
+      {!auth ? (
+        <p onClick={show_modal}>Sign In</p>
+      ) : (
+        <span
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          role="button"
+          tabIndex={0}
+        >
+          <Icon
+            icon={hover.onHover}
+            fill="secondary"
+            width="24"
+            customStyle={{ display: "flex", alignItems: "center" }}
+          />
+        </span>
+      )}
+    </div>
+  );
+};
+
+MenuButton.propTypes = {
+  show_modal: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    show_modal: () => dispatch(show_modal),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(MenuButton);
