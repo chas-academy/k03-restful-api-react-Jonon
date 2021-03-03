2d213e8f102fe6875893900f59ef21ebf957fe70
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-03-03 15:27:56 +0100

    Add check if admin

--- client/src/components/header/menuButton/userMenu/userMenuItems/UserMenuItems.js
+++ client/src/components/header/menuButton/userMenu/userMenuItems/UserMenuItems.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Radium from "radium";
+import { useSelector } from "react-redux";
 // styles
 import UserMenuItemsStyle from "./UserMenuItems.style";
 
 const UserMenuItems = () => {
+  let auth = useSelector((state) => state.authentication.auth);
   return (
     <>
       <li key="0" style={UserMenuItemsStyle}>
@@ -12,9 +14,12 @@ const UserMenuItems = () => {
       <li key="1" style={UserMenuItemsStyle}>
         Profile
       </li>
-      <li key="2" style={UserMenuItemsStyle}>
-        Dashboard
-      </li>
+      {/* Hide Dashboard if not Admin */}
+      {auth.role !== true ? null : (
+        <li key="2" style={UserMenuItemsStyle}>
+          Dashboard
+        </li>
+      )}
     </>
   );
 };
