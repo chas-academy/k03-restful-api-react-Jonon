2d064e0f741d2cc8364b129e589650f1b005f67e
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-03-10 16:10:45 +0100

    Fix order schema

--- api/models/Order.js
+++ api/models/Order.js
@@ -1,12 +1,40 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
-    date: {type: Date, required: true},
-    orderNumber: {type: Number, required: true},
-    orderStatus: {type: String, required: true},
-    total: {type: Number, required: true},
-    quantity: {type: Number, required: true},
-})
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true
+  },
+  product: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date()
+  },
+  orderNumber: {
+    type: mongoose.Schema.ObjectId,
+    required: true
+  },
+  orderStatus: {
+    type: String,
+    default: "Pending",
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1
+  }
+});
 
 module.exports = mongoose.model("Order", orderSchema);
