c4bf4379834f222c3e4e4239b7021a9cce624968
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-03-10 16:02:38 +0100

    Add useNewUrlParser and useUnifiedTopology

--- api/config/db.js
+++ api/config/db.js
@@ -4,13 +4,16 @@ const uri = process.env.DB_CONNECTION;
 
 //connect to DB
 const connectDb = async () => {
-    try {
-        await mongoose.connect(uri, { useNewUrlParser: true });
-        
-        console.log("MongoDb is connected!")
-    } catch (err) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDb is connected!");
+  } catch (err) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 module.exports = connectDb;
