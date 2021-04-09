75e26ca43caf945c49a7902538553b84479f1ead
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-09 19:02:27 +0200

    Add cartDropDown component

--- client/src/components/header/cart/cartDropdown/CartDropdown.js
+++ client/src/components/header/cart/cartDropdown/CartDropdown.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Radium from "radium";
+import CartMenu from "./cartMenu/CartMenu";
+
+//styles
+import CartDropdownStyle from "./CartDropdown.style";
 
 const CartDropdown = () => {
   return (
     <div>
-      <div></div>
+      <div style={CartDropdownStyle.base}>
+        <div style={CartDropdownStyle.arrowAlignment}>
+          <div style={CartDropdownStyle.triangleUp}></div>
+        </div>
+        <div style={CartDropdownStyle.box}>
+          <CartMenu />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/header/cart/cartDropdown/CartDropdown.style.js
+++ client/src/components/header/cart/cartDropdown/CartDropdown.style.js
@@ -1,3 +1,42 @@
-const CartDropdownStyle = {};
+import color from "../../../../styles/colors";
+
+const CartDropdownStyle = {
+  base: {
+    position: "absolute",
+    zIndex: "101",
+    "@media (max-width: 768px)": {
+      top: "3.5rem",
+      left: 0,
+      width: "100%",
+    },
+    "@media (min-width: 768px)": {
+      right: "0.5rem",
+      top: "2.6rem",
+      width: "15rem",
+    },
+  },
+  box: {
+    backgroundColor: color.shades.white00,
+    boxShadow: "1px 0px 4px 0px rgba(0, 0, 0, 0.5)",
+  },
+
+  arrowAlignment: {
+    display: "flex",
+    justifyContent: "flex-end",
+    position: "relative",
+    top: "1rem",
+  },
+  triangleUp: {
+    "@media (min-width: 768px)": {
+      position: "relative",
+      width: 0,
+      height: 0,
+      marginRight: "1.5rem",
+      borderLeft: "0.4rem solid transparent",
+      borderRight: "0.4rem solid transparent",
+      borderBottom: `0.9rem solid ${color.shades.white00}`,
+    },
+  },
+};
 
 export default CartDropdownStyle;
