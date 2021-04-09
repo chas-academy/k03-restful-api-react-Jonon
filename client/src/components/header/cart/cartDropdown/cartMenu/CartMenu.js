dd2da423dc19c1e140dad02cc039f966458b87b3
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-09 19:03:01 +0200

    Add CartMenu component

---
+++ client/src/components/header/cart/cartDropdown/cartMenu/CartMenu.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Components
+import ProductList from "./productList/ProductList";
+
+const CartMenu = () => {
+  return <ProductList />;
+};
+
+export default CartMenu;
