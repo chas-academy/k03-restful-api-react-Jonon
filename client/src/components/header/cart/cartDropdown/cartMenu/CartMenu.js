1017d242b77fa3f22c26b54e6451cf98bb11d172
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-12 20:44:37 +0200

    Add CartTotal component

--- client/src/components/header/cart/cartDropdown/cartMenu/CartMenu.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/CartMenu.js
@@ -1,9 +1,15 @@
 import React from "react";
 // Components
 import ProductList from "./productList/ProductList";
+import CartTotal from "./cartTotal/CartTotal";
 
 const CartMenu = () => {
-  return <ProductList />;
+  return (
+    <div>
+      <ProductList />
+      <CartTotal />
+    </div>
+  );
 };
 
 export default CartMenu;
