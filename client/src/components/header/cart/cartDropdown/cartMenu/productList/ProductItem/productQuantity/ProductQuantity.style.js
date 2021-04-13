2305a50fabc47ec345ba3cd459c99aaefcba556d
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-13 16:47:53 +0200

    add color to border and quantity number

--- client/src/components/header/cart/cartDropdown/cartMenu/productList/ProductItem/productQuantity/ProductQuantity.style.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/productList/ProductItem/productQuantity/ProductQuantity.style.js
@@ -1,14 +1,17 @@
+import color from "../../../../../../../../styles/colors";
+
 const productQuantityStyle = {
   center: {
     display: "flex",
     alignItems: "center",
   },
   border: {
-    border: "1px solid black",
+    border: `1px solid ${color.shades.grey03}`,
     borderRadius: "3px",
   },
   quantity: {
     margin: "0 1rem",
+    color: color.secondary,
   },
 };
 
