595c239ec0a97119d49983335d7d71c4db081a1a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-15 16:16:58 +0200

    Add size property

--- client/src/components/header/cart/cartDropdown/cartMenu/cartTotal/CartTotal.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/cartTotal/CartTotal.js
@@ -11,8 +11,20 @@ const CartTotal = () => {
         <span style={cartTotalStyle.totalTitle}>Total</span>
         <span style={cartTotalStyle.totalPrice}>5.24€</span>
       </div>
-      <Button kind="primary" title="View Cart" rounded="true" marginTop="0.5rem" />
-      <Button kind="primary" title="Checkout" rounded="true" marginTop="0.5rem" />
+      <Button
+        kind="primary"
+        title="View Cart"
+        rounded="true"
+        marginTop="0.5rem"
+        size="md"
+      />
+      <Button
+        kind="primary"
+        title="Checkout"
+        rounded="true"
+        marginTop="0.5rem"
+        size="md"
+      />
     </div>
   );
 };
