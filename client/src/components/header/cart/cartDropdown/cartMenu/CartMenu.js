b45ffc4e0caa6ab7c0cc049497e5ddde50d21a89
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-04-20 19:24:12 +0200

    Import CartButtons and rearange component order

--- client/src/components/header/cart/cartDropdown/cartMenu/CartMenu.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/CartMenu.js
@@ -2,12 +2,14 @@ import React from "react";
 // Components
 import ProductList from "./productList/ProductList";
 import CartTotal from "./cartTotal/CartTotal";
+import CartButtons from "./cartButtons/CartButtons";
 
 const CartMenu = () => {
   return (
     <div>
-      <ProductList />
       <CartTotal />
+      <ProductList />
+      <CartButtons />
     </div>
   );
 };
