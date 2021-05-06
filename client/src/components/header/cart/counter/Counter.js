3f9805d6803044e50912f64b59c93dace23cf617
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-05-06 18:05:04 +0200

    feat: get number of products in cart

--- client/src/components/header/cart/counter/Counter.js
+++ client/src/components/header/cart/counter/Counter.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 // styles
 import counterStyle from "./counterStyle";
 
 const Counter = () => {
+  let cart = useSelector((state) => state.cart);
+
   return (
     <div style={counterStyle.rounded}>
-      <p style={counterStyle.counterTitle}>0</p>
+      <p style={counterStyle.counterTitle}>{cart.totalItems}</p>
     </div>
   );
 };
