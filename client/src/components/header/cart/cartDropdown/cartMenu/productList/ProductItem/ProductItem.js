0dade48dc2671058439fefdca8a767974cc996b7
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-05-06 17:48:07 +0200

    Add functionality to remove Item in cart. Add styling

--- client/src/components/header/cart/cartDropdown/cartMenu/productList/ProductItem/ProductItem.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/productList/ProductItem/ProductItem.js
@@ -5,52 +5,48 @@ import Icon from "../../../../../../../assets/icons";
 import productItemStyle from "./ProductItem.style";
 //components
 import ProductQuantity from "./productQuantity/ProductQuantity";
+import { useSelector, useDispatch } from "react-redux";
 
 const ProductItem = () => {
-  let items = [
-    {
-      id: 0,
-      title: "Vampironica #1 ",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/818279/818279._SX1280_QL80_TTD_.jpg",
-      price: "0.75",
-      quantity: 2,
-    },
-    {
-      id: 1,
-      title: "Silk #1",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/911523/911523._SX1280_QL80_TTD_.jpg",
-      price: "4,49",
-      quantity: 3,
-    },
-  ];
+  let cart = useSelector((state) => state.cart);
+  let dispatch = useDispatch();
+
+  const removeItem = (e) => {
+    let id = e.currentTarget.id;
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
 
   return (
     <>
-      {items.map((item) => (
-        <li>
-          <div style={productItemStyle.gridContainer}>
-            <img style={productItemStyle.poster} src={item.image} />
-            <div style={productItemStyle.contentContainer}>
-              <div style={productItemStyle.flexContainer}>
-                <div style={productItemStyle.title}>{item.title}</div>
-                <Icon
-                  icon="close"
-                  width="24"
-                  fill="primary"
-                  customStyle={productItemStyle.close}
-                />
-              </div>
-              <div style={productItemStyle.flexContainer}>
-                <div style={productItemStyle.price}>{item.price}€</div>
-                <ProductQuantity quantity={item.quantity} />
+      {cart.items.length ? (
+        cart.items.map((item, index) => (
+          <li key={index}>
+            <div style={productItemStyle.gridContainer}>
+              <img style={productItemStyle.poster} src={item.poster} />
+              <div style={productItemStyle.contentContainer}>
+                <div style={productItemStyle.flexContainer}>
+                  <div style={productItemStyle.title}>{item.title}</div>
+                  <div id={item._id} onClick={removeItem}>
+                    <Icon
+                      icon="close"
+                      width="24"
+                      fill="primary"
+                      customStyle={productItemStyle.close}
+                    />
+                  </div>
+                </div>
+                <div style={productItemStyle.flexContainer}>
+                  <div style={productItemStyle.price}>{item.price}€</div>
+                  <ProductQuantity quantity={item.quantity} id={item._id} />
+                </div>
               </div>
             </div>
-          </div>
-          <div style={productItemStyle.dividerLine}></div>
-        </li>
-      ))}
+            <div style={productItemStyle.dividerLine}></div>
+          </li>
+        ))
+      ) : (
+        <p>Cart is empty</p>
+      )}
     </>
   );
 };
