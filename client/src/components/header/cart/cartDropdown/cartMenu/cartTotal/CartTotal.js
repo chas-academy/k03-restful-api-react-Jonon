8abe967ad96b7450e9005924b9630cdbc5676cdd
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-05-06 18:03:10 +0200

    Feat: get total from cartReducer. Make total number have two decimals.

--- client/src/components/header/cart/cartDropdown/cartMenu/cartTotal/CartTotal.js
+++ client/src/components/header/cart/cartDropdown/cartMenu/cartTotal/CartTotal.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // styles
 import cartTotalStyle from "./CartTotal.style";
 
 const CartTotal = () => {
+  let cart = useSelector((state) => state.cart);
+  let total = cart.total;
+  total = total.toFixed(2);
   return (
     <div>
       <div style={cartTotalStyle.totalBox}>
         <span style={cartTotalStyle.totalTitle}>Total</span>
-        <span style={cartTotalStyle.totalPrice}>5.24€</span>
+        <span style={cartTotalStyle.totalPrice}>{total}€</span>
       </div>
       <div style={cartTotalStyle.line}></div>
     </div>
