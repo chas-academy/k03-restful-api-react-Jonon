8e7c7d46a224ea70c44ff0505ce50ecc7f2dd5a8
Jonathan Jonsson <jonathancjonsson@outlook.com>
2021-05-10 20:45:43 +0200

    feat: Add function addToCart

--- client/src/pages/products/product/productDetals/ProductDetals.js
+++ client/src/pages/products/product/productDetals/ProductDetals.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 // Assets
 import Button from "../../../../assets/buttons/Button";
 // styles
 import styles from "./ProductDetals.style";
 
 const ProductDetals = ({ items }) => {
+  let dispatch = useDispatch();
+  let location = useLocation();
+
+  const addToCart = async (e) => {
+    try {
+      e.persist();
+      // Persist path url
+      const response = await fetch(`${location.pathname}`);
+      const data = await response.json();
+      let id = data._id;
+      dispatch({ type: "ADD_TO_CART", payload: { data: data, id: id } });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  console.log(location.pathname);
   return (
     <div>
       <img src={items.poster} style={styles.image} alt="poster" />
@@ -13,7 +32,13 @@ const ProductDetals = ({ items }) => {
         <h4 style={styles.price}>Price</h4>
         <h4 style={styles.price}>{items.price}€</h4>
       </div>
-      <Button title="Add to cart" kind="primary" width="100%" size="lg" />
+      <Button
+        title="Add to cart"
+        kind="primary"
+        width="100%"
+        size="lg"
+        onClick={addToCart}
+      />
     </div>
   );
 };
